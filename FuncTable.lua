4d4ed3bcb38924327da95c3fe1355a8d3e962cf5
ProfBeetle <jthomas@roblox.com>
2016-08-10 19:14:47 -0700

    Since Lexx relies on this, I should include it :)

---
+++ FuncTable.lua
@@ -0,0 +1,52 @@
+    
+    ------------------------------------------------------
+    --
+    --------------------------------- Functional table
+    --
+    -- From http://lua-users.org/wiki/FuncTables
+    --
+    ------------------------------------------------------
+    
+    FuncTable = {}
+    FuncTable.__index = FuncTable
+    function FuncTable.new(values)
+        local self = setmetatable({}, FuncTable)
+        for key, value in pairs (values) do
+            self[key] = value
+        end
+        return self
+    end
+    
+    function FuncTable:each(onEach)
+        for key, value in pairs (self) do
+            onEach(key, value)
+        end
+    end
+    
+    function FuncTable:foldRight(init, onEach)
+        local nextVal = init
+        self:each(function(key, value)
+            nextVal = onEach(nextVal, key, value)
+        end)
+        return nextVal
+    end
+    
+    function FuncTable:map(onEach)
+        local result = self:foldRight(FuncTable.new({}), function(newTable, key, value)
+            newTable[#newTable+1] = onEach(key, value)
+            return newTable
+        end)
+        return result
+    end
+    
+    function FuncTable:filter(onEach)
+        local result = self:foldRight(FuncTable.new({}), function(newTable, key, value)
+            if (onEach(key, value) == 0) then
+                newTable[key] = value
+            end
+            return newTable
+        end)
+        return result
+    end
+
+return FuncTable
